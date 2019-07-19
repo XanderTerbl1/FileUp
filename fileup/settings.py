a207af35f0205b0b0f8f65d3b839a95903c47f1d
XanderTerbl1 <xanderterbl268@gmail.com>
2019-07-19 10:31:56 +0200

    Email hidden

--- fileup/settings.py
+++ fileup/settings.py
@@ -125,11 +125,11 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-# Email Config
+# Email Config (Currently Gmail)
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'fileup25@gmail.com'
-EMAIL_HOST_PASSWORD = 'Testing123!'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Encryption
